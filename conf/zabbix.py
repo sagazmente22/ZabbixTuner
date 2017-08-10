259c053cea37d547e3d1a7d7175f722564959a8b
sagazmente22 <ithalodeoliveira@gmail.com>
2017-08-10 18:01:02 -0300

    Update zabbix.py

--- conf/zabbix.py
+++ conf/zabbix.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 server="http://api.zabbix.qa01.globoi.com"
-username="ithalo.freire"
-password="Sagaz22samurai$"
+username="Seu Usuário"
+password="Sua senha"
 loglevel=0
 timeout=None
